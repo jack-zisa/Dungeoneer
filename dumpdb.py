5b3afb30aaae5406bedf1e85a9596296eb3359a1
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-06-06 20:40:56 -0400

    Update dumpdb.py

--- dumpdb.py
+++ dumpdb.py
@@ -8,23 +8,27 @@ os.makedirs(folder_path, exist_ok=True)
 connection = sqlite3.connect('server/run/dungeoneer.db')
 
 print('Dumping server sessions...')
-sessions_df = pd.read_sql_query("SELECT * FROM server_sessions", connection)
-sessions_df.to_csv('dump_output/server_sessions.csv', index=False)
+df = pd.read_sql_query("SELECT * FROM server_sessions", connection)
+df.to_csv('dump_output/server_sessions.csv', index=False)
 
 print('Dumping client sessions...')
-sessions_df = pd.read_sql_query("SELECT * FROM client_sessions", connection)
-sessions_df.to_csv('dump_output/client_sessions.csv', index=False)
+df = pd.read_sql_query("SELECT * FROM client_sessions", connection)
+df.to_csv('dump_output/client_sessions.csv', index=False)
 
 print('Dumping factions...')
-accounts_df = pd.read_sql_query("SELECT * FROM factions", connection)
-accounts_df.to_csv('dump_output/factions.csv', index=False)
+df = pd.read_sql_query("SELECT * FROM factions", connection)
+df.to_csv('dump_output/factions.csv', index=False)
 
 print('Dumping accounts...')
-accounts_df = pd.read_sql_query("SELECT * FROM accounts", connection)
-accounts_df.to_csv('dump_output/accounts.csv', index=False)
+df = pd.read_sql_query("SELECT * FROM accounts", connection)
+df.to_csv('dump_output/accounts.csv', index=False)
 
 print('Dumping characters...')
-accounts_df = pd.read_sql_query("SELECT * FROM characters", connection)
-accounts_df.to_csv('dump_output/characters.csv', index=False)
+df = pd.read_sql_query("SELECT * FROM characters", connection)
+df.to_csv('dump_output/characters.csv', index=False)
+
+print('Dumping raids...')
+df = pd.read_sql_query("SELECT * FROM raids", connection)
+df.to_csv('dump_output/raids.csv', index=False)
 
 connection.close()
